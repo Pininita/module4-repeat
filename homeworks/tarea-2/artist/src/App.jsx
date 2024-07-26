1c8c31ce8c1f56e8c3c569c6a4625ea9d05deedf
Pininita <pininita92001@gmail.com>
2024-07-25 20:11:36 -0500

    homework-2 finish

---
+++ homeworks/tarea-2/artist/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { ArtistCard } from './components/artistCard'
+
+function App() {
+
+  return (
+    <>
+      <ArtistCard/>
+    </>
+  )
+}
+
+export default App
---
+++ homeworks/tarea-2/artist/src/components/artistCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export function ArtistCard () {
+
+    return(
+        <>
+        <div className="artistCard">
+            <h1>BILLIE EILISH</h1>
+            <img id="artistPic" src='\src\pics\billiePic.jpg' alt="billie-eilsi-pic"/>
+            <p id="artistContent">Billie Eilish is an American singer-songwriter known for her distinctive style and genre-blending music. Born on December 18, 2001, in Los Angeles, California, she gained widespread recognition with her debut single "Ocean Eyes" in 2015. Her debut album, When We All Fall Asleep, Where Do We Go?, released in 2019, topped charts globally and featured hits like "Bad Guy" and "Bury a Friend." Eilish's music often explores themes of mental health, self-reflection, and existentialism, delivered with a unique blend of pop, electronic, and alternative influences. She is known for her ethereal voice, unconventional fashion, and a strong, personal connection with her audience.</p>
+        </div>
+        </>
+    )
+}
