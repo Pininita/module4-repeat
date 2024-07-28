e60aa9941f794a429e4356c5ff4bbf789de996c9
Pininita <pininita92001@gmail.com>
2024-07-28 16:07:33 -0500

    ending character-card from homework3

---
+++ homeworks/tarea-2/artist/src/components/Image.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Image = (props) => {
+
+    return(
+        <img src={props.url} alt="character-image" id="artistPic" />
+    )
+}
--- homeworks/tarea-2/artist/src/components/artistCard.jsx
+++ homeworks/tarea-2/artist/src/components/artistCard.jsx
@@ -1,21 +1,17 @@
 import React from "react";
-
+import rickSanchez from '../pics/rick-sanchez.jpg'
+import { Image } from "./Image";
+import {Title} from "./Title.jsx"
+import { Details } from "./Details";
 
 export function ArtistCard() {
 
     return (
         <>
             <div className="artistCard">
-                <h1>BILLIE EILISH</h1>
-                <img id="artistPic" src='\src\pics\billiePic.jpg' alt="billie-eilsi-pic" />
-                <p id="artistContent">Billie Eilish is an American singer-songwriter known for her
-                    distinctive style and genre-blending music. Born on December 18, 2001, in Los Angeles,
-                    California, she gained widespread recognition with her debut single "Ocean Eyes" in 2015.
-                    Her debut album, When We All Fall Asleep, Where Do We Go?, released in 2019, topped charts
-                    globally and featured hits like "Bad Guy" and "Bury a Friend." Eilish's music often explores
-                    themes of mental health, self-reflection, and existentialism, delivered with a unique blend of
-                    pop, electronic, and alternative influences. She is known for her ethereal voice, unconventional
-                    fashion, and a strong, personal connection with her audience.</p>
+                <Image url={rickSanchez} alt='rick-sanchez-image'/>
+                <Title title='Rick Sanchez'/>
+                <Details status='alive' gender='male'/>
             </div>
         </>
     )
