9dba8912ff172c14aac59bc479afd9b0ad6df5dc
Pininita <pininita92001@gmail.com>
2024-07-31 12:07:58 -0500

    homework4 finish with css style but high-order-component no add yet

--- homeworks/tarea-2/artist/src/App.jsx
+++ homeworks/tarea-2/artist/src/App.jsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from 'react'
 import './App.css'
-import { ArtistCard } from './components/artistCard'
+import { CharacterCard } from './components/CharacterCard';
 
 function App() {
 
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [gender, setGender] = useState("");
+  const [status, setStatus] = useState("");
+
+  const rickAndMortyCharacterId = 1;
+
+  useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character/${rickAndMortyCharacterId}`)
+      .then((reponse) => reponse.json())
+      .then((data) => {
+        console.log(data)
+        setName(data.name)
+        setImage(data.image);
+        setGender(data.gender);
+        setStatus(data.status);
+      })
+      .catch((error) => console.error("error find: ", error))
+  }, [])
+
   return (
     <>
-      <ArtistCard/>
+      <h1>RICK AND MORTY CHARACTERS</h1>
+      <div>
+        <CharacterCard
+          name={name}
+          image={image}
+          gender={gender}
+          status={status}
+        />
+      </div>
+
     </>
   )
 }
--- homeworks/tarea-2/artist/src/components/Image.jsx
+++ homeworks/tarea-2/artist/src/components/Image.jsx
@@ -3,6 +3,8 @@ import React from "react";
 export const Image = (props) => {
 
     return(
-        <img src={props.url} alt="character-image" id="artistPic" />
+        <div className="characterPic">
+        <img src={props.url} alt="character-image" className="image" />
+        </div>
     )
 }
