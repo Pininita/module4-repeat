71d07528ed42212b46a9e4cdbeea34985bbeaa8c
Pininita <pininita92001@gmail.com>
2024-08-10 19:28:18 -0500

    adding search bar with no style

--- homeworks/tarea-2/artist/src/high-order-component/RickAndMortyCharacterCard.jsx
+++ homeworks/tarea-2/artist/src/high-order-component/RickAndMortyCharacterCard.jsx
@@ -3,26 +3,48 @@ import { CharacterCard } from "../components/CharacterCard";
 import { useEffect } from "react";
 import { useState } from "react";
 import { ButtonComponent } from "../components/ButtonComponent";
+import {useLocation} from 'react-router-dom'
 
 export const RickAndMortyCharacterCard = () => {
-    
+
 
     const [characterList, setCharacterList] = useState([]);
     const [offset, setOffset] = useState(1)
 
+    const [nameFilter, setNameFilter] = useState('')
+    const [genderFilter, setGenderFilter] = useState('')
+
+
+
     useEffect(() => {
-        fetch(`https://rickandmortyapi.com/api/character/?page=${offset}`)
+        fetch(`https://rickandmortyapi.com/api/character/?page=${offset}&name=${nameFilter}&gender=${genderFilter}`)
             .then((reponse) => reponse.json())
             .then((data) => {
                 console.log(data)
                 setCharacterList(data.results)
             })
             .catch((error) => console.error("error found: ", error))
-            console.log(offset,'soy offset');
-    }, [offset])
+        console.log(offset, 'soy offset');
+    }, [offset, nameFilter, genderFilter])
+
+        console.log(genderFilter);
+
 
     return (
-       <>
+        <>
+            {/* starting search bar */}
+            <div>
+                <input type="text" id='searchInput' placeholder='Search...' value={nameFilter} onChange={(e) => setNameFilter(e.target.value)} />
+                <select name="" id="gender" value={genderFilter} onChange={(e) => setGenderFilter(e.target.value)}>
+                    <option value="">Select...</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="unknown">Unknown</option>
+                    <option value="genderless">Genderless</option>
+                </select>
+                {/* <button type='button' onClick={handleSearch}>search</button> */}
+            </div>
+            {/* ending search bar */}
             <div className="item-charactercard">
                 {characterList.map((characters) => (
                     <div   key={characters.id}>
@@ -39,12 +61,9 @@ export const RickAndMortyCharacterCard = () => {
             </div>
             <br />
             <section className='button-section-characterpage'>
-                {/* <button>next page</button>
-                {offset}
-                <button>prev page</button> */}
                 <ButtonComponent
-                pages={offset} 
-                setpages={setOffset}
+                    pages={offset}
+                    setpages={setOffset}
                 />
             </section>
         </>
